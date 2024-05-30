66051df928a88f4f8260aad0adcf07ae2210f194
Hashir Saud Khan <hashirsaudkhan@gmail.com>
2024-05-30 04:07:58 -0700

    add theme and sign in page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,15 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:full_food_delivery_app_with_backend/components/my_button.dart';
+import 'package:full_food_delivery_app_with_backend/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final Function()? onTap;
+  const LoginPage({
+    super.key,
+    required this.onTap});
 
   @override
   Widget build(BuildContext context) {
+
+    //Controllers 
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
     return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
         
+          //logo 
+          Icon(
+            Icons.lock_open_rounded,
+            size : 100,
+            color: Theme.of(context).colorScheme.inversePrimary),
+      
+            const SizedBox(height: 25,),
+
+          // message app slogan
+          Text('Food Delivery App',
+          style: TextStyle(
+            fontSize: 16,
+            color: Theme.of(context).colorScheme.inversePrimary
+          ),),
+      
+          const SizedBox(height: 25,),
+
+          // email textfields
+          MyTextfield(controller: emailController, hintText: 'Email', obsecureText: false),
+      
+          const SizedBox(height: 25,),
+
+          //password textfield
+          MyTextfield(controller: passwordController, hintText: 'Password', obsecureText: true),
+
+          const SizedBox(height: 25,),
+
+          //sign in button
+          MyButton(onTap: (){}, text: 'Sign In'),
+
+          const SizedBox(height: 25,),
+      
+          // not a member ? Registor now
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text('Not a member?',style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),),
+
+              const SizedBox(width: 4,),
+              
+            GestureDetector(
+                onTap: onTap,
+                child: Text('Registor now',style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold
+                ),),
+              )
+            ],
+          )
+        ],
       ),
+    ),
     );
+    
   }
 }
---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:full_food_delivery_app_with_backend/themes/dark_mode.dart';
+import 'package:full_food_delivery_app_with_backend/themes/light_mode.dart';
+
+class ThemeProvider with ChangeNotifier{
+
+  // Default theme is set to light mode
+  ThemeData _themeData = lightMode;
+
+  // Getter for accessing the current theme data
+  ThemeData get themeData => _themeData;
+
+  // Getter for checking if the current theme is dark mode
+  bool get isDarkMode => _themeData == darkMode;
+
+  // Setter for updating the theme data
+  set themeData(ThemeData themeData){
+    _themeData = themeData;
+  }
+
+  // Method for toggling between light and dark mode
+  void toggleTheme(){
+    if(_themeData == lightMode){
+       themeData = darkMode; // If current theme is light, switch to dark
+    }else{
+      themeData = lightMode; // If current theme is dark, switch to light
+    }
+  }
+}
